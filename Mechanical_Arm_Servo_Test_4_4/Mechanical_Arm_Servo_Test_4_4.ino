9a8af15a55a04deae4bb629384522cc71e3e8635
a-mahal <arjunmahal5@gmail.com>
2022-04-04 19:48:24 -0400

    Trial Commit

--- Mechanical_Arm_Servo_Test_4_4/Mechanical_Arm_Servo_Test_4_4.ino
+++ Mechanical_Arm_Servo_Test_4_4/Mechanical_Arm_Servo_Test_4_4.ino
@@ -1,26 +1,26 @@
 #include <Servo.h>             /* need this library for servo syntax */
-Servo myservo; 
-     
+Servo myservo;
+
 int servo = 8;                 /* ? */
 long duration, cm, inches;     /* ? */
 
 void setup() {
-Serial.begin(9600);
-pinMode(A0, INPUT);
-pinMode(8,OUTPUT);          
-myservo.attach(8);             /* attaches servo to signal pin on board */
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(8, OUTPUT);
+  myservo.attach(8);             /* attaches servo to signal pin on board */
 
 
 }
 
 void loop() {
 
-int T = analogRead(A0);        /* sets variable T to the analog port on board */
+  int T = analogRead(A0);        /* sets variable T to the analog port on board */
 
-T = map(T,0,1023,0,180);       /* changes analog readings to 0-180 degrees*/
-int A = T; 
-Serial.println(T);             /* lgs what the value of T is */
+  T = map(T, 0, 1023, 0, 180);   /* changes analog readings to 0-180 degrees*/
+  int A = T;
+  Serial.println(T);             /* lgs what the value of T is */
 
-myservo.write(T);              /* controls servo position*/
-
+  myservo.write(T);              /* controls servo position*/
+  /* hello8*/
 }
